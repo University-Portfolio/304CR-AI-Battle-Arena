9184b91d155513c1e982ea587fb19763b52dbae4
Sam Potter <sam_pot_97@hotmail.com>
2018-03-13 16:02:47 +0000

    Added basic stage

---
+++ Assets/Stage/StageController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+	[SerializeField]
+	private float startSize = 10.0f;
+	[SerializeField]
+	private float decayRate = 1.0f;
+
+	public float currentSize { get; private set; }
+
+
+	void Start ()
+	{
+		currentSize = startSize;
+	}
+	
+	void Update ()
+	{
+		currentSize -= decayRate * Time.deltaTime;
+		if (currentSize < 0)
+			currentSize = 0;
+
+		transform.localScale = new Vector3(currentSize, transform.localScale.y, currentSize);
+	}
+}
